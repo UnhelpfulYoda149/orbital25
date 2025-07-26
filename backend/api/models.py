c950f8512ab0f018cd7d3283976d66400685f423
UnhelpfulYoda149 <kohshaobing@gmail.com>
2025-07-26 21:46:24 +0800

    editing variance

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 class Stock(models.Model):
     symbol = models.CharField(max_length=10, primary_key=True)
     name = models.CharField(max_length=255)
-    var = models.FloatField(default=0)
 
     def __str__(self):
         return self.name
